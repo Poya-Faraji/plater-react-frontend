e4c24c81b1a4c53794b3274943e51fb44a243115
Poya Faraji <poya.faraji@outlook.com>
2025-07-03 17:02:01 +0330

    Fix styles applying globally

--- src/assets/components/NotFoundPage/notFound.jsx
+++ src/assets/components/NotFoundPage/notFound.jsx
@@ -1,7 +1,7 @@
 import "./notFound.css"
 export default function NotFoundPage() {
   return (
-    <div>
+    <div className="notFound">
       <svg
         id="svgWrap_2"
         xmlns="http://www.w3.org/2000/svg"
